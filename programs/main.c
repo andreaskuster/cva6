7cb7cc37c8fa98f9237fba91d3cb1a1d309b4763
andreaskuster <mail@andreaskuster.ch>
2022-01-07 14:48:11 +0100

    Refactor, cleanup code.

--- programs/main.c
+++ programs/main.c
@@ -43,6 +43,11 @@
 #define DMA_CONF_DEBURST 0
 #define DMA_CONF_SERIALIZE 0
 
+
+#define TEST_TRAP 0
+#define TEST_PMP_SRC 0
+#define VERBOSE 1
+
 #define ASSERT(expr, msg)             \
 if (!(expr)) {                        \
     print_uart("assertion failed: "); \
@@ -51,212 +56,158 @@ if (!(expr)) {                        \
     return -1;                        \
 }
 
-static inline void sleep_loop(){
-
-    for(int i = 0; i < 16*DMA_TRANSFER_SIZE; i++){ 
-        asm volatile ("ADDI x0, x1, 0" :  : ); // nop operation
-        //asm volatile ("nop" :  : ); // nop operation
+static inline void sleep_loop() {
+    for (int i = 0; i < 16 * DMA_TRANSFER_SIZE; i++) {
+        asm volatile ("nop" :  : ); // nop operation
     }
 }
 
 int main(int argc, char const *argv[]) {
 
-    // asm volatile("li sp, 0x81000000"); // set up stack
-
+    /*
+     * Setup uart
+     */
     init_uart(50000000, 115200);
     print_uart("Hello CVA6 from iDMA!\n");
 
-
+    /*
+     * Setup trap
+     */
     setup_trap();
 
+    if (TEST_TRAP) { // cause null ptr exception
+        volatile uintptr_t *null_ptr = (volatile uintptr_t *) 0x0;
+        print_uart_int(*null_ptr);
+    }
 
-
-//   volatile uint64_t* test = (volatile uint64_t*)0x0;
-//   uint64_t asfd = *test;
-//    print_uart_int(asfd);
     /*
      * Setup relevant configuration registers
      */
-    volatile uint64_t* dma_src = (volatile uint64_t*)DMA_SRC_ADDR;
-    volatile uint64_t* dma_dst = (volatile uint64_t*)DMA_DST_ADDR;
-    volatile uint64_t* dma_num_bytes = (volatile uint64_t*)DMA_NUMBYTES_ADDR;
-    volatile uint64_t* dma_conf = (volatile uint64_t*)DMA_CONF_ADDR;
-    //volatile uint64_t* dma_status = (volatile uint64_t*)DMA_STATUS_ADDR;
-    volatile uint64_t* dma_nextid = (volatile uint64_t*)DMA_NEXTID_ADDR;
-    volatile uint64_t* dma_done = (volatile uint64_t*)DMA_DONE_ADDR;
-
-
+    volatile uint64_t *dma_src = (volatile uint64_t *) DMA_SRC_ADDR;
+    volatile uint64_t *dma_dst = (volatile uint64_t *) DMA_DST_ADDR;
+    volatile uint64_t *dma_num_bytes = (volatile uint64_t *) DMA_NUMBYTES_ADDR;
+    volatile uint64_t *dma_conf = (volatile uint64_t *) DMA_CONF_ADDR;
+    // volatile uint64_t* dma_status = (volatile uint64_t*)DMA_STATUS_ADDR; // not used in current implementation
+    volatile uint64_t *dma_nextid = (volatile uint64_t *) DMA_NEXTID_ADDR;
+    volatile uint64_t *dma_done = (volatile uint64_t *) DMA_DONE_ADDR;
 
     /*
      * Prepare data
      */
-    // allocate DMA_TRANSFER_SIZE bytes
+    // allocate source array
     uint64_t src[DMA_TRANSFER_SIZE / sizeof(uint64_t)];
-    //uint64_t guard[512]; // 4KB guard band
-    uint64_t dst[DMA_TRANSFER_SIZE / sizeof(uint64_t)];
+    if (VERBOSE) {
+        // print array stack address
+        print_uart("Source array @0x");
+        print_uart_addr((uint64_t) & src);
+        print_uart("\n");
+    }
 
-    // [debug] print stack address
-    // print_uart("charptr@");
-    // print_uart_addr(&src);
-    // print_uart("\n");
+    // 1KB guard
+    char __attribute__((unused)) guard[1 << 10];
+
+    // allocate destination array
+    uint64_t dst[DMA_TRANSFER_SIZE / sizeof(uint64_t)];
+    if (VERBOSE) {
+        // print array stack address
+        print_uart("Destination array @0x");
+        print_uart_addr((uint64_t) & dst);
+        print_uart("\n");
+    }
 
     // fill src array & clear dst array
-    for(size_t i = 0; i < DMA_TRANSFER_SIZE / sizeof(uint64_t); i++){
+    for (size_t i = 0; i < DMA_TRANSFER_SIZE / sizeof(uint64_t); i++) {
         src[i] = 42;
         dst[i] = 0;
     }
+    // TODO: flush cache?
 
     /*
      * PMP: Guard source data
      */
-
-    // pmpcfg_t pmp0 = {
-    //   .cfg = ,
-    //   .a0 = ,
-    //   .a1 = 
-    // }
-
-//    detect_pmp();
+    detect_pmp();
     detect_granule();
     set_pmp_zero();
-/*
-    // try protecting non-cached region instead
-    // pmpcfg_t pmp0 = set_pmp_range((uintptr_t)UART_BASE, 16);
-    pmpcfg_t pmp0 = set_pmp_napot((uintptr_t)UART_BASE, 16);    
-    test_one((uintptr_t)UART_BASE, 4);
-    
-    //write_reg_u8(UART_BASE, 'a');
-    //read_reg_u8(UART_BASE);
-*/
-
-/*
-    // whitelist dst array region
-    pmpcfg_t pmp0 = set_pmp_napot((uintptr_t)&dst, DMA_TRANSFER_SIZE);
-    //pmpcfg_t pmp0 = set_pmp_range((uintptr_t)&src, DMA_TRANSFER_SIZE);
-    test_one((uintptr_t)&dst, 4); // TODO: <- this should work
-    
-    //test_one((uintptr_t)&src, 4); // TODO: <- access violation
-*/
-
-    //set_pmp_napot_access((uintptr_t)&src, DMA_TRANSFER_SIZE, PMP_R | PMP_W | PMP_X);
-    //set_pmp_napot_access((uintptr_t)&src, DMA_TRANSFER_SIZE, PMP_NO_ACCESS, 1);
-    set_pmp_napot_access((uintptr_t)&src, DMA_TRANSFER_SIZE, (PMP_R | PMP_W | PMP_X), 1); // allow src access for testing purpose
-    set_pmp_napot_access((uintptr_t)&dst, DMA_TRANSFER_SIZE, PMP_NO_ACCESS, 2); //
-    //test_one((uintptr_t)&dst, 8); // TODO: <- this should work
-    //print_uart("reading dst worked :)\n");
-    //test_one((uintptr_t)&src, 8); // TODO: <- access violation
-    //print_uart("reading src worked :( \n");
-
-    //pmpcfg_t pmp1 = set_pmp_range(&src, 8);//DMA_TRANSFER_SIZE);
-    //test_one(&src, 8);// DMA_TRANSFER_SIZE);
-    //src[0] = 42;
-    
-    // for(int i = 0; i < 10; i++){
-    //     print_uart("try reading address ");
-    //     print_uart_int(&src[i]);
-    //     print_uart(" value: ");
-    //     print_uart_int(src[i]);
-    //     print_uart("\n");
-    // }
+    // block access to source array
+    set_pmp_napot_access((uintptr_t) & src, DMA_TRANSFER_SIZE, PMP_NO_ACCESS, 1);
+    // however, allow access to destination array
+    set_pmp_napot_access((uintptr_t) & dst, DMA_TRANSFER_SIZE, (PMP_R | PMP_W | PMP_X), 2);
 
     /*
      * Test register access
      */
     print_uart("Test register read/write\n");
-    
-    // *dma_src = 42;
-    // int test = *dma_src;
-    // //while(1);
-    // print_uart("dma_src written\n");
-    
-    // print_uart("dma_src: ");
-    // print_uart_int(*dma_src);
-    // print_uart("\n");
-    // ASSERT(*dma_src == 42, "");
 
-    // TODO: enable
-    // *dma_src = 42;
-    // *dma_dst = 42;
-    // *dma_num_bytes = 0;
-    // *dma_conf = 7;   // 0b111
+    // test register read/write
+    *dma_src = 42;
+    *dma_dst = 42;
+    *dma_num_bytes = 0;
+    *dma_conf = 7;   // 0b111
 
-    // ASSERT(*dma_src == 42, "dma_src");
-    // ASSERT(*dma_dst == 42, "dma_dst");
-    // ASSERT(*dma_num_bytes == 0, "dma_num_bytes");
-    // ASSERT(*dma_conf == 7, "dma_conf");
+    ASSERT(*dma_src == 42, "dma_src");
+    ASSERT(*dma_dst == 42, "dma_dst");
+    ASSERT(*dma_num_bytes == 0, "dma_num_bytes");
+    ASSERT(*dma_conf == 7, "dma_conf");
 
     /*
      * Test DMA transfer
      */
     print_uart("Initiate dma request\n");
 
-    // TODO: flush cache?
-
     // setup src to dst memory transfer
-    *dma_src = (uint64_t)&src;
-    *dma_dst = (uint64_t)&dst;
+    *dma_src = (uint64_t) & src;
+    *dma_dst = (uint64_t) & dst;
     *dma_num_bytes = DMA_TRANSFER_SIZE;
-    *dma_conf = (DMA_CONF_DECOUPLE << DMA_FRONTEND_CONF_DECOUPLE_BIT) | (DMA_CONF_DEBURST << DMA_FRONTEND_CONF_DEBURST_BIT) | (DMA_CONF_SERIALIZE << DMA_FRONTEND_CONF_SERIALIZE_BIT);
+    *dma_conf = (DMA_CONF_DECOUPLE << DMA_FRONTEND_CONF_DECOUPLE_BIT) |
+                (DMA_CONF_DEBURST << DMA_FRONTEND_CONF_DEBURST_BIT) |
+                (DMA_CONF_SERIALIZE << DMA_FRONTEND_CONF_SERIALIZE_BIT);
 
     print_uart("Start transfer\n");
 
     // launch transfer: read id
-    //uint64_t transfer_id  = 0;
-
-    //for(int i = 0;3 > i; i++){
-    uint64_t transfer_id = *dma_nextid; // = DMA_TRANSFER_ID;
+    uint64_t transfer_id = *dma_nextid;
 
     // work-around: add delay to free axi bus (axi_node does not allow parallel transactions -> need to upgrade axi xbar)
-    // sleep_loop();
-    for(int i = 0; i < 16*DMA_TRANSFER_SIZE; i++){ 
-        //asm volatile ("ADDI x0, x1, 0" :  : ); // nop operation
+    for (int i = 0; i < 16 * DMA_TRANSFER_SIZE; i++) {
         asm volatile ("nop" :  : ); // nop operation
     }
-
 
     // poll wait for transfer to finish
     do {
         print_uart("Transfer finished: ");
         print_uart("transfer_id: ");
-        print_uart_int(transfer_id);         
+        print_uart_int(transfer_id);
         print_uart(" done_id: ");
-        print_uart_int(*dma_done);        
+        print_uart_int(*dma_done);
         print_uart(" dst[0]: ");
         print_uart_int(dst[0]);
         print_uart("\n");
-    } while(*dma_done != transfer_id);
+    } while (*dma_done != transfer_id);
 
     // TODO: invalidate cache?
 
     // check result
-    for(size_t i = 0; i < DMA_TRANSFER_SIZE / sizeof(uint64_t); i++){
+    for (size_t i = 0; i < DMA_TRANSFER_SIZE / sizeof(uint64_t); i++) {
 
-        uintptr_t dst_val = read_pmp_locked((uintptr_t)&(dst[i]), 8); //dst[i];//;
-        print_uart("Try reading dst: ");
+        uintptr_t dst_val = read_pmp_locked((uintptr_t) & (dst[i]), 8); //dst[i];//;
+        print_uart("Try reading dst: 0x");
         print_uart_int(dst_val);
-        print_uart("\n");       
-
-        // uintptr_t src_val = read_pmp_locked((uintptr_t)&(src[i]), 8);//dst[i];//;
-        // print_uart("try reading src: ");
-        // print_uart_int(src_val);
-        // print_uart("\n");          
-
-        // test_one((uintptr_t)&dst, 8); // TODO: <- this should work
-        // print_uart("reading dst worked :)\n");
-        // test_one((uintptr_t)&src, 8); // TODO: <- access violation
-        // print_uart("reading src worked :( \n");
-
+        print_uart("\n");
 
         ASSERT(dst_val == 42, "dst");
+
+        if (TEST_PMP_SRC) {
+            uintptr_t src_val = read_pmp_locked((uintptr_t) & (src[i]), 8); //dst[i];//;
+            print_uart("Try reading src: 0x");
+            print_uart_int(src_val);
+            print_uart("\n");
+        }
+
     }
     print_uart("Transfer successfully validated.\n");
 
-    // // free allocated memory
-    // free(src);
-    // free(dst);
-    
     print_uart("All done, spin-loop.\n");
-    while (1){
+    while (1) {
         // do nothing
     }
 
